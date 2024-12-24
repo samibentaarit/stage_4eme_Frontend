d518962de77476d4f54d9fdfc13eaf986eb44033
samibentaarit <sbentaarit@gmail.com>
2024-12-24 06:49:12 +0100

    fix: Correct spelling of "Annances" to "Annonces" in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
                     : "text-white hover:bg-indigo-600"
                 }`}
               >
-                Annances
+                Annonces
               </Link>
             </li>
             <li>
